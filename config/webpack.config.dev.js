daefa905cc8ca9c8300accef3ed2e9ba9a2dc974
sunboy <38577739+hyxieshi@users.noreply.github.com>
2022-11-17 17:35:31 +0800

    ceshi

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,1 +1,10 @@
-士大夫首发式
+/*
+ * @Author: SunBOY
+ * @Date: 2022-11-17 17:24:01
+ * @LastEditors: SunBOY
+ * @LastEditTime: 2022-11-17 17:24:22
+ * @FilePath: \config\webpack.config.dev.js
+ * @Description:
+ * Copyright 2022 OBKoro1, All Rights Reserved.
+ * 2022-11-17 17:24:01
+ */
